784937e851dfd6f06670a7bb51f02e9fa0e180b4
Dlayknee <Dlayknee@gmail.com>
2020-04-27 11:44:20 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '$ssh.rsa.key',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDgZhMsXZeYfdkwBH5W5z7VjiVj0Yd7D44l3uDc8KBGkAa3HeumMRzUBaEpBaQi4PcREVFZTeCqn9kcJ27qze6RYrhUD/hX+z6EYnf1IA5rIiOF1PboVg+GLsJPaYgTAeh7i7JY9Kwfj/sx3BPj7XBz+EUcZHY1FRB/XSRgqJkyFB4p3lZkNySQDaf9k2b08JAMKQaJ2K4exJgw5pXuvzvfiRiKWowl1o4VGfFk4tgnNEUiHZfiiuItuxS6jmWgv5o2igdZDuf7/2efwjWoLFkcgNqZd93TP/IDhwKZwQ7/YA8l2QReo0qtfSQFGNxwhalkijj6vPgoCSfbceI4Jrw/',
 	}  
 }
